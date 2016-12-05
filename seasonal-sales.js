653e001fec67d1551040cfc38c34ec0428dd54f7
j-west <westja89@gmail.com>
2016-12-05 12:21:11 -0600

    Add variables to hold div with an id of wrapper and a newly created div to append.  add for loop to add values in array from JSON file to created div

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,21 +1,39 @@
 var productsRequest = new XMLHttpRequest();
+var productDiv = document.createElement("div");
+var wrapperDiv = document.querySelector("#wrapper");
 
 function getProductInfo(e) {
   var products = JSON.parse(e.target.responseText);
   console.log(products);
+
+for (var i = 0; i < products.products.length; i++) {
+  currentProduct = products.products[i];
+  productDiv.innerHTML += ` <h1>${currentProduct.name}</h1>
+                            <p>Department: </p>
+                            <p>Price: ${currentProduct.price}</p>
+                          `
+  wrapperDiv.appendChild(productDiv);
+
+}
+  console.log(productDiv);
+
 }
 
 productsRequest.addEventListener("load", getProductInfo);
 productsRequest.open("GET", "products.json");
 productsRequest.send();
 
+
+
+//Categories JSON file
+
 var categoriesRequest = new XMLHttpRequest();
 
-function getProductInfo(e) {
-  var categories = JSON.parse(e.target.responseText);
+function getCategoryInfo(e) {
+ var categories = JSON.parse(e.target.responseText);
   console.log(categories);
 }
 
-categoriesRequest.addEventListener("load", getProductInfo);
+categoriesRequest.addEventListener("load", getCategoryInfo);
 categoriesRequest.open("GET", "categories.json");
 categoriesRequest.send();
